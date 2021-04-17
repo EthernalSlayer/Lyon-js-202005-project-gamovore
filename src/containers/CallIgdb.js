3485311bd8e339bd617ab4114d91495546e307ab
EthernalSlayer <guillaume.b.a@protonmail.com>
2021-04-17 22:59:55 +0200

    add oauth authentification

--- src/containers/CallIgdb.js
+++ src/containers/CallIgdb.js
@@ -1,34 +1,45 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+let API_KEY = '';
 
 const CallIgdb = (dataCallIgdb) => {
-  const [gameList, setGameList] = useState([]);
-  const [loading, setLoading] = useState(true);
+	const [gameList, setGameList] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    axios({
-      url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "user-key": API_KEY,
-      },
-      data: dataCallIgdb,
-    })
-      .then((response) => response.data)
-      .then((data) => {
-        setGameList(data.sort((a, b) => (a.name > b.name ? 1 : -1)));
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setLoading(false);
-      });
-  }, [dataCallIgdb]);
+	useEffect(() => {
+		console.log(API_KEY);
+		axios
+			.post(
+				`https://id.twitch.tv/oauth2/token?client_id=${process.env.REACT_APP_API_CLIENT_ID}&client_secret=${process.env.REACT_APP_API_SECRET}&grant_type=client_credentials`
+			)
+			.then((response) => response.data)
+			.then((data) => (API_KEY = data.access_token))
+			.catch((err) => console.log(err));
+		if (API_KEY.length) {
+			axios({
+				url: 'https://api.igdb.com/v4/games',
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					Client_ID: process.env.REACT_APP_API_CLIENT_ID,
+					Authorization: `Bearer ${API_KEY}`,
+				},
+				// data: dataCallIgdb,
+			})
+				.then((response) => response.data)
+				.then((data) => {
+					setGameList(data.sort((a, b) => (a.name > b.name ? 1 : -1)));
+					setLoading(false);
+				})
+				.catch((err) => {
+					console.log(err);
+					setLoading(false);
+				});
+		}
+	}, [dataCallIgdb, API_KEY]);
 
-  return { gameList, setGameList, loading, setLoading };
+	return { gameList, setGameList, loading, setLoading };
 };
 
 export default CallIgdb;
